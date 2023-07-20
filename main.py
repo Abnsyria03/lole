e7585465f7020885f9d40294276b8c3e73eccaa4
Raqawi <134682323+Abnsyria03@users.noreply.github.com>
2023-07-20 20:39:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pyromod import listen
 from pyrogram import Client, filters, enums
 from kvsqlite.sync import Client as dt
 p = dict(root='plugins')
-tok = '5455927541:AAEpC5mRbYaoDP5ExRDVM8XP3FuUnscgkfU' ## توكنك 
+tok = '5455927541:AAGutpSENYG_QCx6HGR03TyfqO0xaASl0AU' ## توكنك 
 id = 5108562302 ## ايديك
 db = dt("data.sqlite", 'fuck')
 if not db.get("checker"):
